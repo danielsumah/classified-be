1536fcec182d114316a7fa22aa08eafd027bdfa8
danielsumah <52764894+danielsumah@users.noreply.github.com>
2025-03-08 14:02:47 +0100

    cors setup in main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,22 @@ import { ConfigService } from '@nestjs/config';
 import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const logger = new Logger()
-  const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService);
-  const port = configService.get('PORT');
-  await app.listen(port);
-  logger.log(`\n\nApplication is running on port: ${port}\n`);
+ const logger = new Logger();
+ const app = await NestFactory.create(AppModule);
+ const configService = app.get(ConfigService);
+ 
+ const port = configService.get('PORT');
+ const devFrontendUrl = configService.get('DEV_FRONTEND_URL');
+ const localFrontendUrl = configService.get('LOCAL_FRONTEND_URL');
+ 
+ app.enableCors({
+   origin: [devFrontendUrl, localFrontendUrl],
+   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+   credentials: true,
+ });
+ 
+ await app.listen(port);
+ logger.log(`\n\nApplication is running on port: ${port}\n`);
+ logger.log(`\n\nAllowed origins: ${devFrontendUrl}, ${localFrontendUrl}\n`);
 }
 bootstrap();
