b974ef79622dc99eacfdc8bb77cc52ddebbae42a
danielsumah <52764894+danielsumah@users.noreply.github.com>
2025-03-08 20:12:32 +0100

    mongo db setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from './datasource/typeorm.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true, // Makes ConfigModule global
+    }),
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+        dbName: configService.get<string>('MONGODB_DB_NAME'),
+      }),
     }),
     TypeOrmModule,
     UsersModule],
